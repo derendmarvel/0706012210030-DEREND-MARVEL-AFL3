f999ec3b255c124b0fb7cf611e400876a82640fc
Derend <derend101@gmail.com>
2024-04-19 07:37:44 +0700

    Fixing Section 5

--- Landmarks/Views/MapView.swift
+++ Landmarks/Views/MapView.swift
@@ -8,11 +8,12 @@
 import SwiftUI
 import MapKit
 
+
 struct MapView: View {
     var body: some View {
         Map(initialPosition: .region(region))
     }
-    
+
     private var region: MKCoordinateRegion {
         MKCoordinateRegion(
             center: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868),
@@ -20,6 +21,7 @@ struct MapView: View {
         )
     }
 }
+
 
 #Preview {
     MapView()
