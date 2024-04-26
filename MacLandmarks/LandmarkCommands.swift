cc1c4dcc6d556f6fe7fdc2418f9e63088da7b811
Derend <derend101@gmail.com>
2024-04-26 09:52:31 +0700

    Chapter 4 Part 3 Section 5

---
+++ MacLandmarks/LandmarkCommands.swift
@@ -0,0 +1,14 @@
+//
+//  LandmarkCommands.swift
+//  Landmarks
+//
+//  Created by student on 26/04/24.
+//
+
+import SwiftUI
+
+struct LandmarkCommands: Commands{
+    var body: some Commands{
+        SidebarCommands()
+    }
+}
